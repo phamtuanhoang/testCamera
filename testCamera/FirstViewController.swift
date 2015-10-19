f8e9aeab294304f7ef5cac0e9d1fa7a6049579f0
phamtuanhoang <phamtuanhoang_1612@yahoo.com>
2015-10-19 09:58:57 +0800

    Initial Commit

---
+++ testCamera/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  testCamera
+//
+//  Created by tuanhoang pham on 19/10/15.
+//  Copyright © 2015 tuanhoang pham. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
