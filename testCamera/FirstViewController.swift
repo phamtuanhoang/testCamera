fffaf5f9a2c9f928b03d1edc3f14a3e7de7e30d2
phamtuanhoang <phamtuanhoang_1612@yahoo.com>
2015-10-19 18:00:03 +0800

    first commit

--- testCamera/FirstViewController.swift
+++ testCamera/FirstViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    var photoHelper : PhotoHelper?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tabBarController?.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func takePhoto(){
+        //instantiate photo taking class, provide callback
+        let photoHelper = PhotoHelper(viewController: self.tabBarController!) { (image: UIImage?) -> Void in
+            //using trailing closure
+            print("in photo helper class")
+        }
+    }
 }
+
+
+extension FirstViewController: UITabBarControllerDelegate{
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is SecondViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
 
---
+++ testCamera/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  testCamera
+//
+//  Created by tuanhoang pham on 19/10/15.
+//  Copyright © 2015 tuanhoang pham. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController,UITabBarControllerDelegate
+{
+    
+        
+}
+
